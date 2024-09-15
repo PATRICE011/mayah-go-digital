8c396c5f6ae50c710591ca286a29e4cd462fda70
tony <itslapada1124@gmail.com>
2024-09-15 19:44:07 +0800

    modified nav bar

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,52 +1,52 @@
 
 <header class="header" id="header">
-         <nav class="nav container">
-            <a href="index.php" class="nav__logo">
-                <img src="assets/img/MAYAH-STORE-LOGO.jpg" alt="" class="nav__logo-img">
-                Mayah Store
-            </a>
+   <nav class="nav container">
+      <a href="index.php" class="nav__logo">
+          <img src="assets/img/MAYAH-STORE-LOGO.jpg" alt="" class="nav__logo-img">
+          Mayah Store
+      </a>
 
-            <div class="nav__menu" id="nav-menu">
-               <ul class="nav__list">
-                  <li class="nav__item">
-                     <a href="{{route('home.index')}}" class="nav__link active-link">Home</a>
-                  </li>
+      <div class="nav__menu" id="nav-menu">
+         <ul class="nav__list">
+            <li class="nav__item">
+               <a href="{{route('home.index')}}" class="nav__link active-link">Home</a>
+            </li>
+            
+            <li class="nav__item">
+               <a href="#about" class="nav__link">About Us</a>
+            </li>
 
-                  <li class="nav__item">
-                     <a href="#about" class="nav__link">About Us</a>
-                  </li>
+            <li class="nav__item">
+               <a href="#products" class="nav__link">Products</a>
+            </li>
 
-                  <li class="nav__item">
-                     <a href="#products" class="nav__link">Products</a>
-                  </li>
+            <li class="nav__item">
+               <a href="#" class="nav__link"></a>
+            </li>
+         </ul>
 
-                  <li class="nav__item">
-                     <a href="#" class="nav__link"></a>
-                  </li>
-               </ul>
+         <!-- Close button -->
+         <div class="nav__close" id="nav-close">
+            <i class="ri-close-line"></i>
+         </div>
+      </div>
 
-               <!-- Close button -->
-               <div class="nav__close" id="nav-close">
-                  <i class="ri-close-line"></i>
-               </div>
-            </div>
+      <div class="nav__actions">
+         <!-- Search button -->
+         <i class="ri-search-line nav__search" id="search-btn"></i>
 
-            <div class="nav__actions">
-               <!-- Search button -->
-               <i class="ri-search-line nav__search" id="search-btn"></i>
+         <!-- Cart button -->
+         <div class="nav_shop" id="cart-shop">
+             <i class="ri-shopping-cart-2-line nav__shop"></i>
+         </div>
 
-               <!-- Cart button -->
-                <div class="nav_shop" id="cart-shop">
-                    <i class="ri-shopping-cart-2-line nav__shop"></i>
-                    
-                </div>
-               <!-- Login button -->
-               <i class="ri-user-line nav__login" id="login-btn"></i>
-               
-               <!-- Toggle button -->
-               <div class="nav__toggle" id="nav-toggle">
-                  <i class="ri-menu-line"></i>
-               </div>
-            </div>
-         </nav>
-      </header>
+         <!-- Login button -->
+         <i class="ri-user-line nav__login" id="login-btn"></i>
+
+         <!-- Toggle button -->
+         <div class="nav__toggle" id="nav-toggle">
+            <i class="ri-menu-line"></i>
+         </div>
+      </div>
+   </nav>
+</header>
