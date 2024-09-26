f34311beac8de708ebd5077e4f6ef3faccf837da
tony <itslapada1124@gmail.com>
2024-09-26 11:22:45 +0800

    fixed header, still has bugs

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,15 +1,15 @@
-
 <header class="header" id="header">
    <nav class="nav container">
-      <a href="index.php" class="nav__logo">
+      <a href="{{route('home.index')}}" class="nav__logo">
           <!-- <img src="assets/img/MAYAH-STORE-LOGO.jpg" alt="" class="nav__logo-img"> -->
-          <i class="ri-leaf-line nav__logo-icon"></i> Mayah Store
+          <i class="ri-leaf-line nav__logo-icon"></i>
+          Mayah Store
       </a>
 
       <div class="nav__menu" id="nav-menu">
          <ul class="nav__list">
             <li class="nav__item">
-               <a href="{{route('home.index')}}" class="nav__link">Home</a>
+               <a href="{{route('home.index')}}" class="nav__link active-link">Home</a>
             </li>
             
             <li class="nav__item">
@@ -23,9 +23,12 @@
             <li class="nav__item">
                <a href="#faqs" class="nav__link">FAQs</a>
             </li>
-
+            
             <li class="nav__item">
-               <a href="#" class="nav__link"></a>
+               <a href="#faqs" class="nav__link">
+                  <!-- Login button -->
+                  <!-- <i class="ri-user-line nav__login" id="login-btn"></i> User -->
+               </a>
             </li>
          </ul>
 
@@ -35,21 +38,20 @@
          </div>
       </div>
 
-      <div class="nav__actions">
-         <!-- Search button -->
-         <i class="ri-search-line nav__search" id="search-btn"></i>
+      <div class="nav__btns">
+         <!-- Theme change button -->
+         <i class="ri-moon-line change-theme" id="theme-button"></i>
 
          <!-- Cart button -->
          <div class="nav_shop" id="cart-shop">
              <i class="ri-shopping-cart-2-line nav__shop"></i>
          </div>
 
-         <!-- Login button -->
-         <i class="ri-user-line nav__login" id="login-btn"></i>
+         <!-- Search button -->
+         <i class="ri-search-line nav__search" id="search-btn"></i>
 
-         <!-- Toggle button -->
          <div class="nav__toggle" id="nav-toggle">
-            <i class="ri-menu-line"></i>
+             <i class="ri-menu-line"></i>
          </div>
       </div>
    </nav>
