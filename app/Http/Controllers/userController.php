75c97b6df031c65282f997ef9dc16f10f3ae8e91
pat <quitoles0906@gmail.com>
2024-12-09 19:07:21 +0800

    filter product

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Cart;
 use App\Models\CartItem;
+use Illuminate\Http\Request;
 
 
 class userController extends Controller
@@ -105,5 +106,60 @@ class userController extends Controller
             'cartCount' => $cartCount,
             'wishlistCount' => $wishlistCount
         ]);
+    }
+    public function filterProducts(Request $request)
+    {
+        $categories = $request->input('categories', []);
+        $products = DB::table('products')
+            ->join('categories', 'products.category_id', '=', 'categories.id')
+            ->select('products.*', 'categories.category_name')
+            ->when(!empty($categories), function ($query) use ($categories) {
+                $query->whereIn('categories.category_name', $categories);
+            })
+            ->get();
+        // Count total products in the database
+        $totalProducts = DB::table('products')->count();
+        // Generate the HTML for filtered products
+        $html = '';
+        foreach ($products as $product) {
+            $html .= '
+                <div class="product__item">
+                    <div class="product__banner">
+                        <a href="#" class="product__images">
+                            <img src="' . asset('assets/img/' . $product->product_image) . '" alt="' . $product->product_name . '" class="product__img default">
+                            <img src="' . asset('assets/img/' . $product->product_image) . '" alt="' . $product->product_name . '" class="product__img hover">
+                        </a>
+                        <div class="product__actions">
+                            <a href="' . url('/details') . '" class="action__btn" aria-label="Quick View">
+                                <i class="bx bx-expand-horizontal"></i>
+                            </a>
+                            <a href="#" class="action__btn" aria-label="Add To Wishlist">
+                                <i class="bx bx-heart"></i>
+                            </a>
+                        </div>
+                        <div class="product__badge light-pink">Hot</div>
+                    </div>
+                    <div class="product__content">
+                        <span class="product__category">' . $product->category_name . '</span>
+                        <a href="details.html">
+                            <h3 class="product__title">' . $product->product_name . '</h3>
+                        </a>
+                        <div class="product__price flex">
+                            <span class="new__price">₱ ' . number_format($product->product_price, 2) . '</span>
+                            <span class="old__price">₱ 9.00</span>
+                        </div>
+                        
+                        <form action="' . route('home.inserttocart') . '" method="POST" class="d-inline">
+                            ' . csrf_field() . '
+                            <input type="hidden" name="id" value="' . $product->id . '">
+                            <button type="submit" class="action__btn cart__btn" aria-label="Add To Cart">
+                                <i class="bx bx-cart-alt"></i>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            ';
+        }
+        return response()->json(['html' => $html, 'count' => $products->count(), 'total' => $totalProducts]);
     }
 }
