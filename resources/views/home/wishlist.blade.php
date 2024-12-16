f86dbe642864ebe2d829d8045b168c54728b9718
pat <quitoles0906@gmail.com>
2024-12-16 19:54:46 +0800

    ok na ung sa details

--- resources/views/home/wishlist.blade.php
+++ resources/views/home/wishlist.blade.php
@@ -158,11 +158,19 @@
 
                <td>
                   @if ($wishlistItem->product->product_stocks > 0)
-                  <a href="{{ route('home.inserttocart', $wishlistItem->product->id) }}" class="btn btn--sm">Add to Cart</a>
+                  <form action="{{ route('home.inserttocart') }}" method="POST" class="d-inline">
+                     @csrf
+                     <input type="hidden" name="id" value="{{ $wishlistItem->product->id }}">
+                     <input type="hidden" name="quantity" value="1">
+                     <button type="button" class="btn btn--sm action__btn cart__btn">
+                        Add to Cart
+                     </button>
+                  </form>
                   @else
                   <button class="btn btn--sm" disabled>Out of Stock</button>
                   @endif
                </td>
+
 
                <td>
                   <form id="destroy-button" action="{{ route('wishlist.remove',$wishlistItem->id) }}" method="POST">
