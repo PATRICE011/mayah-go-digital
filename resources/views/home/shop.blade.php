756190f74c75b9face60250435618dd92996a1f5
pat <quitoles0906@gmail.com>
2025-04-05 21:45:12 +0800

    fixed shop problem

--- resources/views/home/shop.blade.php
+++ resources/views/home/shop.blade.php
@@ -46,7 +46,7 @@
       <a href="{{url('/')}}" class="nav__logo">
          <i class="ri-restaurant-2-fill nav__logo-icon"></i> Mayah Store
       </a>
-      @auth
+      @endauth
 
       <div class="nav__menu" id="nav-menu">
          <div class="nav__menu-top">
